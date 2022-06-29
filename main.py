067addde4dbbdbe561342ee2e5f480605e1ba755
owain-williams <owain@andersdigital.com>
2022-06-29 20:43:22 +0100

    Timer now working

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 from tkinter import *
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 2
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 3
 reps = 0
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    reps = 0
+    timer_label.config(text="timer", fg=GREEN)
+    start_butt.config(command=start_timer)
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
+    start_butt.config(command="")
     global reps
+    reps +=1
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    count_down(2 * 60)
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def count_down(count):
+    if reps % 8 == 0:
+        timer_label.config(text="break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text="break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        timer_label.config(text="work", fg=GREEN)
+        count_down(work_sec)
 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
     min_display = math.floor(count / 60)
     sec_display = count % 60
     if sec_display < 10:
@@ -31,20 +48,17 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{min_display}:{sec_display}")
     if count > 0:
-        print(min_display)
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-
-
-
-
-
 
 # Title
 timer_label = Label(text="timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 42, "italic"))
@@ -61,12 +75,11 @@ canvas.grid(row=2, column=1)
 start_butt = Button(text="Start", width=12, pady=5, command=start_timer)
 start_butt.grid(row=3, column=0)
 
-restart_butt = Button(text="Restart", width=12, pady=5)
+restart_butt = Button(text="Restart", width=12, pady=5, command=reset_timer)
 restart_butt.grid(row=3, column=2)
 
 # Tick marks
 tick_marks = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 18, "italic"))
 tick_marks.grid(row=4, column=1)
-
 
 window.mainloop()
